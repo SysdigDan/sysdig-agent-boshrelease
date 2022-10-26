3276152f975bb1f9485adf83da76bf29fa21d16e
Dan Moloney <daniel.moloney@sysdig.com>
2022-10-26 18:00:26 +1100

    add scripts

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,15 +1,21 @@
 set -e
 
-TMP_FILES=./tmp
+TMP_DIR=tmp
 AGENT_VERSION=12.9.0
 
-mkdir -p $files_dir
-cd $TMP_FILES
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 
+# Download Sysdig Agent
 if [ "$1" != "--skip-download" ]; then
-  # Download Sysdig Agent
   apt-get -qq install apt-transport-https
   curl -s https://download.sysdig.com/DRAIOS-GPG-KEY.public | apt-key add -
 	curl -s -o /etc/apt/sources.list.d/draios.list https://download.sysdig.com/stable/deb/draios.list
-  apt-get -qq update && apt-get -qq install --download-only -o Dir::Cache="/tmp/" draios-agent=${AGENT_VERSION}
+  apt-get -qq update && apt-get -qq install --download-only draios-agent=${AGENT_VERSION}
+  cp /var/cache/apt/archives/draios-agent_${AGENT_VERSION}_amd64.deb .
+fi
+
+# Download Sysdig Agent
+if [ "$1" != "--skip-download" ]; then
+  wget -q -O ./sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic.ko https://s3.amazonaws.com/download.draios.com/stable/sysdig-probe-binaries/sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic-d8051e959d836ee23665f1d33702220a.ko
 fi
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 AGENT_VERSION=12.9.0
-RELEASE_DEV_VERSION=1+dev.21
+RELEASE_DEV_VERSION=1+dev.22
 
-bosh add-blob scripts/draios-agent_${AGENT_VERSION}_amd64.deb sysdig-agent/draios-agent_${AGENT_VERSION}_amd64.deb
-bosh add-blob scripts/sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic.ko sysdig-agent/sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic.ko
+bosh add-blob tmp/draios-agent_${AGENT_VERSION}_amd64.deb sysdig-agent/draios-agent_${AGENT_VERSION}_amd64.deb
+bosh add-blob tmp/sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic.ko sysdig-agent/sysdigcloud-probe-${AGENT_VERSION}-x86_64-4.15.0-191-generic.ko
 
-bosh create-release --force --name "sysdig-agent" --version=${RELEASE_DEV_VERSION=1+dev.21} --tarball=/tmp/sysdig-agent-release-${RELEASE_DEV_VERSION=1+dev.21}.tgz
-scp -P 2222 /tmp/sysdig-agent-release-${RELEASE_DEV_VERSION=1+dev.21}.tgz admin@192.168.101.101://volume1/web
+bosh create-release --force --name "sysdig-agent" --version=${RELEASE_DEV_VERSION=1+dev.21} --tarball=tmp/sysdig-agent-release-${RELEASE_DEV_VERSION=1+dev.21}.tgz
+scp -P 2222 tmp/sysdig-agent-release-${RELEASE_DEV_VERSION=1+dev.21}.tgz admin@192.168.101.101://volume1/web
