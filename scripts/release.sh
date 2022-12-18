5c767b7367be8e252945e0b28ff714fd76cf5d7c
Daniel Moloney <daniel.moloney@sysdig.com>
2022-12-19 09:33:24 +1100

    Release Sysdig Agent 12.10.0-1

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 RELEASE="sysdig-agent"
-SYSDIG_AGENT_VERSION=12.9.1-3
-RELEASE_DEV_VERSION=${SYSDIG_AGENT_VERSION}+dev.1
+SYSDIG_AGENT_VERSION=12.10.0
+RELEASE_VERSION=1
+DEV_VERSION=dev.1
 
 TMP_DIR=tmp/
 mkdir -p ${TMP_DIR}
@@ -19,24 +20,27 @@ if [ ! -f "/usr/local/bin/bosh" ]; then
   export PATH="${WORKING_DIR}/bin:${PATH}"
 fi
 
-# run the prepare script
-./prepare
+# run the prepare-build script
+./scripts/prepare-build ${SYSDIG_AGENT_VERSION}
 bosh sync-blobs
 
 if [ "$1" = "--dev" ]; then
   # release a dev version of the agent
-  bosh create-release --force --name ${RELEASE} --version=${RELEASE_DEV_VERSION} --tarball=tmp/sysdig-agent-release-${RELEASE_DEV_VERSION}.tgz
+  bosh create-release --force --name ${RELEASE} --version=${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}+${DEV_VERSION} --tarball=tmp/sysdig-agent-release-${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}+${DEV_VERSION}.tgz
 
   # upload to archive storage
-  scp -P 2222 tmp/sysdig-agent-release-${RELEASE_DEV_VERSION}.tgz admin@192.168.101.101://volume1/web
+  scp -P 2222 tmp/sysdig-agent-release-${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}+${DEV_VERSION}.tgz admin@192.168.101.101://volume1/web
 else
   # release a dev version of the agent to ensure the cache is warm
   # (it's better to fail here than to fail when really attempting to release it)
-  bosh create-release --force --name ${RELEASE} --version=${RELEASE_DEV_VERSION}
+  bosh create-release --force --name ${RELEASE} --version=${SYSDIG_AGENT_VERSION}-${DEV_VERSION}
 
   # finally, release the agent
-  bosh create-release --force --final --name ${RELEASE} --version=${SYSDIG_AGENT_VERSION} --tarball=tmp/sysdig-agent-release-${SYSDIG_AGENT_VERSION}.tgz
+  bosh create-release --force --final --name ${RELEASE} --version=${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION} --tarball=tmp/sysdig-agent-release-${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}.tgz
   bosh upload-blobs
+
+  # upload to archive storage
+  scp -P 2222 tmp/sysdig-agent-release-${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}.tgz admin@192.168.101.101://volume1/web
 
   # config git
   git config --global user.email "daniel.moloney@sysdig.com"
@@ -45,6 +49,9 @@ else
 
   # git commit it and then push it to the repo
   git add .
-  git commit -m "Release Sysdig Agent ${SYSDIG_AGENT_VERSION}"
+  git commit -m "Release Sysdig Agent ${SYSDIG_AGENT_VERSION}-${RELEASE_VERSION}"
   git push
+
+  # update release version
+  echo ${SYSDIG_AGENT_VERSION} > ./version
 fi
